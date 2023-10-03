4f762aec3b87bd6bf70595c17109a8e23f983471
JULIEN BONET <julienbonetpro@gmail.com>
2023-10-03 15:47:30 +0200

    modif info film (btn en savoir + et switch) / modif btn up show / hidden

--- recipe_switch.js
+++ recipe_switch.js
@@ -1,3 +1,5 @@
+///// SWITCH INGREDIENT / PREPARATION /////
+
 // SELECTIONS DES BOUTON ---------------------------------------------
 
     // Bouton PREPARATION
@@ -18,9 +20,6 @@ const PrepaSide = document.querySelector('.preparation_box');
 
     // SIDE INGREDIENTS
 const IngreSide = document.querySelector('.ingredients_box');
-
-
-
 
 
 // gestion des évènement au click 
@@ -53,11 +52,36 @@ ingreButtonBox.addEventListener("click", function(){
     prepaButtonTitle.style.display ="block";
     ingreButtonBox.style.display ="none";
     ingreButtonTitle.style.display ="none";
-    
-
-
     });
 
 
 
+///// SWITCH info + /////
 
+///selection du bouton [en savoir +]
+const Button_movie_infos_off = document.querySelector('#movie_info_button_closed');
+const Button_movie_infos_on = document.querySelector('#movie_info_button_open');
+
+
+/// selection Div Movie info
+const movie_infos = document.querySelector('.movie_infos')
+const movie_infos_container= document.querySelector('.movie_infos_container')
+
+/// gestion evenement au click
+
+Button_movie_infos_off.addEventListener("click", function(){
+    movie_infos.style.display ="initial";
+    movie_infos_container.style.width = "90%";
+    movie_infos_container.style.padding = "0 0 0 3em";
+
+
+    Button_movie_infos_off.style.display ="none";
+    Button_movie_infos_on.style.display ="flex";
+    });
+
+Button_movie_infos_on.addEventListener("click", function(){
+    movie_infos.style.display ="none";
+    
+    Button_movie_infos_off.style.display ="flex";
+    Button_movie_infos_on.style.display ="none";
+    });
--- up_btn.js
+++ up_btn.js
@@ -9,3 +9,17 @@ btn.addEventListener('click', () => {
     })
 
 })
+
+let derniere_position_de_scroll_connue = 0;
+window.addEventListener("scroll", function (e) {
+    derniere_position_de_scroll_connue = window.scrollY;
+    console.log("derniere_position_de_scroll_connue",derniere_position_de_scroll_connue)
+   if (derniere_position_de_scroll_connue > 900) {
+      btn.style.display ="flex";
+    }
+    if (derniere_position_de_scroll_connue < 900) {
+        btn.style.display ="none";
+      }
+})
+
+
